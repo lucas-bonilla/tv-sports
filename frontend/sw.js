0e0c0fe44e883b626d1dc66282a3a554d1eecf83
lucas-bonilla <lukasbcmlg@gmail.com>
2026-06-03 12:51:53 +0200

    fix: removed cal icon

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const STATIC_CACHE = "sports-tv-static-v12";
+const STATIC_CACHE = "sports-tv-static-v13";
 const API_CACHE = "sports-tv-api-v1";
 const STATIC = ["/", "/index.html", "/style.css", "/app.js", "/padel.js", "/manifest.json"];
 
