10ffd28a1ee9b7ba45e74ebc0ff8179326f93576
lucas-bonilla <lukasbcmlg@gmail.com>
2026-07-03 12:41:42 +0200

    feat: updated to v29

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const STATIC_CACHE = "sports-tv-static-v28";
+const STATIC_CACHE = "sports-tv-static-v29";
 const API_CACHE = "sports-tv-api-v1";
 const STATIC = ["/", "/index.html", "/style.css", "/app.js", "/padel.js", "/worldcup.js", "/manifest.json"];
 
